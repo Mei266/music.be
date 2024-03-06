c5a78b804dceb93e9465aa719caade5972bf528b
Mei266 <A4hainamtys1@gmail.com>
2024-03-06 21:05:18 +0700

    fix artist

--- music/serializers/artist.py
+++ music/serializers/artist.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from music.models import Artist
+from music.serializers import MusicSerializers
 
 
 class ArtistSerializers(serializers.ModelSerializer):
-
+    music_list = serializers.SerializerMethodField()
+    
     class Meta:
         model = Artist
-        fields = "__all__"
+        fields = ["id","name", "image", "music_list"]
         ordering = ["create_at"]
+    
+    def get_music_list(self, obj):
+        artist = Artist.objects.get(pk=obj.id)
+        music_list = artist.artist_item.all()
+        serializer = MusicSerializers(music_list, many=True)
+        return serializer.data
